ab264515b0c267c71b440688aa7359ff5cf0972a
lucenarapereira <93477361+lucenarapereira@users.noreply.github.com>
2022-01-09 16:11:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+/*Exercicio 2 da lista */
+
+class Retangulo {
+    constructor (L, a) {
+        this.largura = L
+        this.altura = a
+    }
+
+
+calcularArea(){
+      const area = this.largura * this.altura;
+      return area.toFixed(2);
+    }
+  };
+
+  console.log(Json.stringify(area))
